2844c1c070e60c08d539cd47b5ddcd155e9a4416
Knut Aldrin <buz@samfundet.no>
2021-05-16 11:54:24 +0200

    Rubocop style fixes

--- lib/monsterid.rb
+++ lib/monsterid.rb
@@ -21,53 +21,53 @@ class MonsterID
     'mouth_S5.png', 'mouth_S4.png'
   ]
   SPECIFIC_COLOR_PARTS = {
-    'arms_S2.png'  => [0, 200],
-    'hair_S6.png'  => [0, 200],
-    'mouth_9.png'  => [0, 200],
-    'mouth_6.png'  => [0, 200],
+    'arms_S2.png' => [0, 200],
+    'hair_S6.png' => [0, 200],
+    'mouth_9.png' => [0, 200],
+    'mouth_6.png' => [0, 200],
     'mouth_S2.png' => [0, 200]
   }
-  
+
   PARTS = {
-    :arms  => [
+    arms: [
       'arms_1.png', 'arms_2.png', 'arms_3.png', 'arms_4.png', 'arms_5.png',
       'arms_S1.png', 'arms_S2.png', 'arms_S3.png', 'arms_S4.png', 'arms_S5.png',
       'arms_S6.png', 'arms_S7.png', 'arms_S8.png', 'arms_S9.png'
     ],
-    :body  => [
+    body: [
       'body_1.png', 'body_2.png', 'body_3.png', 'body_4.png', 'body_5.png',
       'body_6.png', 'body_7.png', 'body_8.png', 'body_9.png', 'body_10.png',
       'body_11.png', 'body_12.png', 'body_13.png', 'body_14.png', 'body_15.png',
       'body_S1.png', 'body_S2.png', 'body_S3.png', 'body_S4.png', 'body_S5.png'
     ],
-    :eyes  => [
+    eyes: [
       'eyes_1.png', 'eyes_2.png', 'eyes_3.png', 'eyes_4.png', 'eyes_5.png',
       'eyes_6.png', 'eyes_7.png', 'eyes_8.png', 'eyes_9.png', 'eyes_10.png',
       'eyes_11.png', 'eyes_12.png', 'eyes_13.png', 'eyes_14.png', 'eyes_15.png',
       'eyes_S1.png', 'eyes_S2.png', 'eyes_S3.png', 'eyes_S4.png', 'eyes_S5.png'
     ],
-    :hair  => [
+    hair: [
       'hair_1.png', 'hair_2.png', 'hair_3.png', 'hair_4.png', 'hair_5.png',
       'hair_S1.png', 'hair_S2.png', 'hair_S3.png', 'hair_S4.png', 'hair_S5.png',
       'hair_S6.png', 'hair_S7.png'
     ],
-    :legs  => [
+    legs: [
       'legs_1.png', 'legs_2.png', 'legs_3.png', 'legs_4.png', 'legs_5.png',
       'legs_S1.png', 'legs_S10.png', 'legs_S11.png', 'legs_S12.png',
-      'legs_S13.png', 'legs_S2.png', 'legs_S3.png', 'legs_S4.png', 
+      'legs_S13.png', 'legs_S2.png', 'legs_S3.png', 'legs_S4.png',
       'legs_S5.png', 'legs_S6.png', 'legs_S7.png', 'legs_S8.png', 'legs_S9.png'
     ],
-    :mouth => [
+    mouth: [
       'mouth_1.png', 'mouth_2.png', 'mouth_3.png', 'mouth_4.png', 'mouth_5.png',
       'mouth_6.png', 'mouth_7.png', 'mouth_8.png', 'mouth_9.png', 'mouth_10.png',
       'mouth_S1.png', 'mouth_S2.png', 'mouth_S3.png', 'mouth_S4.png', 'mouth_S5.png',
       'mouth_S6.png', 'mouth_S7.png'
     ]
   }
-  
+
   # Remove body 12 for now, doesn't look good
-  PARTS[:body].delete( 'body_12.png' )
-  
+  PARTS[:body].delete('body_12.png')
+
   # Test color set [hue, sat]
   COLORS = [
     [8, 255], [160, 64], [8, 127], [184, 255],
@@ -75,52 +75,50 @@ class MonsterID
     [48, 64], [248, 191], [72, 127], [272, 64],
     [128, 255], [312, 191], [160, 255], [336, 255]
   ]
-  
+
   def initialize(seed, size = 120)
     @id = Digest::SHA1.hexdigest seed.to_s
-    
+
     parts = {
-      :legs  => {
-        :part => PARTS[:legs ][@id[0, 2].hex % PARTS[:legs ].length],
-        :color => COLORS[@id[2, 2].hex % COLORS.length]
+      legs: {
+        part: PARTS[:legs][@id[0, 2].hex % PARTS[:legs].length],
+        color: COLORS[@id[2, 2].hex % COLORS.length]
       },
-      :hair  => {
-        :part => PARTS[:hair ][@id[4, 2].hex % PARTS[:hair ].length],
-        :color => COLORS[@id[6, 2].hex % COLORS.length]
+      hair: {
+        part: PARTS[:hair][@id[4, 2].hex % PARTS[:hair].length],
+        color: COLORS[@id[6, 2].hex % COLORS.length]
       },
-      :arms  => { 
-        :part => PARTS[:arms ][@id[8, 2].hex % PARTS[:arms ].length],
-        :color => COLORS[@id[10, 2].hex % COLORS.length]
+      arms: {
+        part: PARTS[:arms][@id[8, 2].hex % PARTS[:arms].length],
+        color: COLORS[@id[10, 2].hex % COLORS.length]
       },
-      :body  => {
-        :part => PARTS[:body ][@id[12, 2].hex % PARTS[:body ].length],
-        :color => COLORS[@id[14, 2].hex % COLORS.length]
+      body: {
+        part: PARTS[:body][@id[12, 2].hex % PARTS[:body].length],
+        color: COLORS[@id[14, 2].hex % COLORS.length]
       },
-      :eyes  => {
-        :part => PARTS[:eyes ][@id[16, 2].hex % PARTS[:eyes ].length],
-        :color => COLORS[@id[18, 2].hex % COLORS.length]
+      eyes: {
+        part: PARTS[:eyes][@id[16, 2].hex % PARTS[:eyes].length],
+        color: COLORS[@id[18, 2].hex % COLORS.length]
       },
-      :mouth => {
-        :part => PARTS[:mouth][@id[20, 2].hex % PARTS[:mouth].length],
-        :color => COLORS[@id[22, 2].hex % COLORS.length]
+      mouth: {
+        part: PARTS[:mouth][@id[20, 2].hex % PARTS[:mouth].length],
+        color: COLORS[@id[22, 2].hex % COLORS.length]
       }
     }
-    
+
     @monster = ChunkyPNG::Image.new(120, 120, ChunkyPNG::Color::TRANSPARENT)
-    
+
     parts.each do |_, part|
       path = File.join(File.dirname(__FILE__), 'parts', part[:part])
       partimg = ChunkyPNG::Image.from_file(path)
-      
+
       if BODY_COLOR_PARTS.include? part[:part]
         part[:color] = parts[:body][:color]
       elsif SPECIFIC_COLOR_PARTS.include? part[:part]
         part[:color] = SPECIFIC_COLOR_PARTS[part[:part]]
       end
-      
-      unless WHITE_PARTS.include? part[:part]
-        partimg = colorise(partimg, part[:color][0], part[:color][1])
-      end
+
+      partimg = colorise(partimg, part[:color][0], part[:color][1]) unless WHITE_PARTS.include? part[:part]
 
       @monster.compose!(partimg)
     end
@@ -139,15 +137,13 @@ class MonsterID
   def to_datastream
     @monster.to_datastream
   end
-  
-  def id
-    @id
-  end
+
+  attr_reader :id
 
   def to_data_url
     @monster.to_data_url
   end
-  
+
   def inspect
     "#<#{self.class.name}:#{object_id} id: #{@id}>"
   end
--- monsterid.gemspec
+++ monsterid.gemspec
@@ -1,29 +1,28 @@
 Gem::Specification.new do |s|
-  s.name = "monsterid"
-  s.version = File.read("VERSION").strip
-  s.require_paths = ["lib"]
-  s.authors = ["Knut Aldrin"]
-  s.description = "Port/rework of the php script, with new monsters snatched from the WP plugin"
-  s.email = "knutaldrin@gmail.com"
+  s.name = 'monsterid'
+  s.version = File.read('VERSION').strip
+  s.require_paths = ['lib']
+  s.authors = ['Knut Aldrin']
+  s.description = 'Port/rework of the php script, with new monsters snatched from the WP plugin'
+  s.email = 'knutaldrin@gmail.com'
   s.extra_rdoc_files = [
-    "LICENSE",
-    "README.md"
+    'LICENSE',
+    'README.md'
   ]
   s.files = [
-    "LICENSE",
-    "README.md",
-    "VERSION",
-    "CHANGELOG",
-    "lib/monsterid.rb",
-  ] + Dir.glob("lib/parts/*.png")
+    'LICENSE',
+    'README.md',
+    'VERSION',
+    'CHANGELOG',
+    'lib/monsterid.rb'
+  ] + Dir.glob('lib/parts/*.png')
 
-  s.homepage = "https://github.com/knutaldrin/monsterid"
-  s.licenses = ["CC-BY-4.0"]
-  s.summary = "Generates tiny little monsters to discern users."
-  
+  s.homepage = 'https://github.com/knutaldrin/monsterid'
+  s.licenses = ['CC-BY-4.0']
+  s.summary = 'Generates tiny little monsters to discern users.'
+
   s.add_runtime_dependency 'chunky_png', '~> 1.4'
-  
+
   s.add_development_dependency 'bundler', '~> 2.1'
   s.add_development_dependency 'rubocop', '~> 1.14'
 end
-
